95e96e3472492a45b44ce4ba9d54383f84298128
Nina Matti <ninadesireematti@gmail.com>
2020-10-14 18:05:16 +0900

    Added resolvers for types, fastattacks and attacks
    Co-Authored-By: Clay <klayxi@gmail.com>

--- server/resolvers.js
+++ server/resolvers.js
@@ -7,7 +7,20 @@ module.exports = {
       return data.pokemon;
     },
     Pokemon: (parent, args) => {
-      return data.pokemon.find((pokemon) => pokemon.name === args.name);
-   },
+      if (isNaN(Number(args.id))) {
+        return data.pokemon.find((pokemon) => pokemon.name === args.name);
+      } else {
+        return data.pokemon.find((pokemon) => pokemon.id === args.id);
+      }
+    },
+    Types: (parent, args) => {
+      return data.types;
+    },
+    FastAttacks: (parent, args) => {
+      return data.attacks.fast;
+    },
+    Attacks: (parent, args) => {
+      return data.attacks;
+    },
   },
 };
