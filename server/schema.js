f552f8a389e73286e26842047e9566bcda8353fd
Nina Matti <ninadesireematti@gmail.com>
2020-10-14 16:02:24 +0900

    Wrote out the necessary types for all pokemon data
    Co-Authored-By: Clay <klayxi@gmail.com>

--- server/schema.js
+++ server/schema.js
@@ -4,10 +4,65 @@ const typeDefs = gql`
   # Your schema goes here.
   # The schema should model the full data object available!
 
-  type Pokemon {
-    id: String
-    name: String!
+  type Weight {
+    minimum: String
+    maximum: String
   }
+
+  type Height {
+    minimum: String
+    maximum: String
+  }
+
+  type PrevEvolutions {
+    id: Int
+    name: String
+  }
+
+  type evolRequirements {
+    amount: Int
+    name: String
+  }
+
+  type Evolutions {
+    id: Int
+    name: String
+  }
+
+  type fastAttacks {
+    name: String
+    type: String
+    damage: Int
+  }
+
+  type specialAttacks {
+    name: String
+    type: String
+    damage: Int
+  }
+
+  type Attacks {
+    fast: [fastAttacks]
+    special: [specialAttacks]
+  }
+
+  type Pokemon {
+    id: String!
+    name: String!
+    classification: String
+    types: [String]
+    resistant: [String]
+    weight: Weight
+    height: Height
+    fleeRate: Float
+    previousEvolutions: [PrevEvolutions]
+    evolutionRequirements: evolRequirements
+    evolutions: [Evolutions]
+    maxCP: Int
+    maxHP: Int
+    attacks: Attacks
+  }
+
   type Query {
     Pokemons: [Pokemon]
     Pokemon(name: String!): Pokemon
